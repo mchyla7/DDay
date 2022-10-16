414358814a55173b37e602026066442d2750ae65
m_chyla <mchyla7@gmail.com>
2022-10-16 16:01:57 +0200

    Počeštěné dny a hodiny a minuty a sekundy

--- js/functions.js
+++ js/functions.js
@@ -103,7 +103,7 @@ function timeElapse(c) {
 	if (f < 10) {
 		f = "0" + f
 	}
-	var a = '<span class="digit">' + g + '</span> days <span class="digit">' + b + '</span> hours <span class="digit">' + d + '</span> minutes <span class="digit">' + f + "</span> seconds";
+	var a = '<span class="digit">' + g + '</span> dní <span class="digit">' + b + '</span> hodin <span class="digit">' + d + '</span> minut <span class="digit">' + f + "</span> sekund";
 	$("#elapseClock").html(a)
 }
 
